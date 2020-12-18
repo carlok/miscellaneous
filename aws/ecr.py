36419b3ae73114ea52f0fc67769364f8b18e2c73
Carlo Perassi <carlo.perassi@gmail.com>
2020-12-18 09:45:42 +0100

    c and aws ecr, tag setter, initial

--- aws/ecr.py
+++ aws/ecr.py
@@ -3,3 +3,22 @@ import boto3
 def ecr_tags_get(credentials, arn):
 	ecr = boto3.client('ecr', region_name=credentials['AWS_REGION'])
 	return ecr.list_tags_for_resource(resourceArn=arn)
+
+def ecr_tag_update(region, account_id, repo):
+    ecr = boto3.client(
+        'ecr',
+        region_name=region
+    )
+
+    arn = 'arn:aws:ecr:{}:{}:repository/{}'.format(region, account_id, repo['name'])
+    repository_tags = ecr.list_tags_for_resource(resourceArn=arn)
+
+    response = ecr.tag_resource(
+        resourceArn=arn,
+        tags=[
+            {
+                'Key': 'stable_tag',
+                'Value': repo['tag']
+            },
+        ]
+    )
